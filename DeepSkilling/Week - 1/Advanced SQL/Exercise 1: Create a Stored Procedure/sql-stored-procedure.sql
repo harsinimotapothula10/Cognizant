cdebc1d2be7efd7cad12985969e4b772fa38748b
harsinimotapothula10 <harsinimotapothula@gmail.com>
2026-07-21 18:59:29 +0530

    Add files via upload

---
+++ DeepSkilling/Week - 1/Advanced SQL/Exercise 1: Create a Stored Procedure/sql-stored-procedure.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE sp_GetEmployeeCountByDepartment(
+    IN p_DepartmentID INT
+)
+BEGIN
+    SELECT COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = p_DepartmentID;
+END $$
+
+DELIMITER ;
+
+CALL sp_GetEmployeeCountByDepartment(1);
